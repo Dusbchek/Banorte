8ab378b989907050fe6682ade0fec764335ca1c0
Dubschek <dinosaurdubs@gmail.com>
2025-02-25 22:59:28 -0600

    rutas API GET y POST

---
+++ routes/api.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+
+
+
+Rounte::group(["middleware" => ["auth:santum"]],function(){
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post("/logout", function (Request $request){
+
+        $request->$user()->currentAccessToken()->delete();
+
+        return response()->noContent();
+
+        Route::get('/transactions', function () {
+            $transactions = DB::table('transactions')->get();
+            return response()->json($transactions);
+        });
+    
+        Route::get('/special_sections', function () {
+            $specialSections = DB::table('special_sections')->get();
+            return response()->json($specialSections);
+        });
+    
+        Route::get('/daily_incomes', function () {
+            $dailyIncomes = DB::table('daily_incomes')->get();
+            return response()->json($dailyIncomes);
+        });
+    
+        Route::get('/daily_expenses', function () {
+            $dailyExpenses = DB::table('daily_expenses')->get();
+            return response()->json($dailyExpenses);
+        });
+    
+        Route::get('/investments', function () {
+            $investments = DB::table('investments')->get();
+            return response()->json($investments);
+        });
+    
+        Route::get('/investments_results', function () {
+            $investmentsResults = DB::table('investments_results')->get();
+            return response()->json($investmentsResults);
+        });
+});
+
+
+} );
+
+Route::post('/login', function (Request $request) {
+   
+    $request->validate([
+        'name' => 'required|string|max:255',
+    ]);
+
+
+    $user = User::where('name', $request->name)->first();
+
+ 
+    if (!$user) {
+        return response()->json([
+            'error' => 'El usuario no existe. Intenta nuevamente.'
+        ], 404); 
+    }
+
+    $token = $user->createToken('YourAppName')->plainTextToken;
+
+    // Devolver la respuesta con el token
+    return response()->json([
+        'token' => $token,
+        'user' => $user,  
+    ]);
+});
